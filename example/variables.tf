ecd2de444ee9d3cc297af72de2bfe9be36225da9
megum1n <misaka@pantsu.moe>
2025-01-31 13:20:18 +0100

    Remove defaults

--- example/variables.tf
+++ example/variables.tf
@@ -7,35 +7,29 @@ variable "mongo_host" {
 variable "mongo_username" {
   description = "MongoDB admin username"
   type        = string
-  default     = ""
 }
 
 variable "mongo_password" {
   description = "MongoDB admin password"
   type        = string
-  default     = ""
 }
 
 variable "database_name" {
   description = "Database name"
   type        = string
-  default     = "test"
 }
 
 variable "role_name" {
   description = "MongoDB role name"
   type        = string
-  default     = "testRole"
 }
 
 variable "user_username" {
   description = "New MongoDB username"
   type        = string
-  default     = ""
 }
 
 variable "user_password" {
   description = "New MongoDB user password"
   type        = string
-  default     = ""
 }
