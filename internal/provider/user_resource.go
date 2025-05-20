f794a7392de39e8f1de2428a0618c0a76d22f121
megum1n <misaka@pantsu.moe>
2025-05-20 10:19:51 +0200

    fix(users): Add default for mechanisms attribute

--- internal/provider/user_resource.go
+++ internal/provider/user_resource.go
@@ -10,6 +10,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/setdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
@@ -144,6 +145,7 @@ func (r *UserResource) Schema(_ context.Context, _ resource.SchemaRequest, resp 
 				ElementType: types.StringType,
 				Optional:    true,
 				Computed:    true,
+				Default:     setdefault.StaticValue(types.SetNull(types.StringType)),
 			},
 		},
 	}
